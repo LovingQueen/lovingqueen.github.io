859a498c18d6a332f3444a1c8752e0729e337ad1
Luke <lukesaccento@icloud.com>
2023-08-05 00:04:39 -0700

    Caroselelsoussol text

--- carousel.js
+++ carousel.js
@@ -1,28 +1,33 @@
-var slides = document.getElementsByClassName("slide");
+var slidesImg = document.getElementsByClassName("slideImg");
+var slidesTxt = document.getElementsByClassName("slideTxt");
 let index = 0;
 
 function prevSlide() {
-    slides[index].classList.remove('active');
+    slidesImg[index].classList.remove('active');
+    slidesTxt[index].classList.remove('active');
     index--;
 
     if (index < 0)
-        index = slides.length - 1;
+        index = slidesImg.length - 1;
 
-    slides[index].classList.add('active');
+    slidesImg[index].classList.add('active');
+    slidesTxt[index].classList.add('active');
 }
 
 function nextSlide() {
-    slides[index].classList.remove('active');
+    slidesImg[index].classList.remove('active');
+    slidesTxt[index].classList.remove('active');
     index++;
 
-    if (index > slides.length - 1)
+    if (index > slidesImg.length - 1)
         index = 0;
 
-    slides[index].classList.add('active');
+    slidesImg[index].classList.add('active');
+    slidesTxt[index].classList.add('active');
 }
 
 function startCarousel() {
-    setInterval(nextSlide, 3000); // Change slide every 3 seconds (adjust as needed)
+    setInterval(nextSlide, 4000); // Change slide every 3 seconds (adjust as needed)
 }
 
 startCarousel(); // Start the automatic carousel movement
