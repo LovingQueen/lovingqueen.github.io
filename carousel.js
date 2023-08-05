2662ec99eb5c2af877b00bdc5468c5b988b13f0d
Luke <lukesaccento@icloud.com>
2023-08-04 22:57:04 -0700

    its working kinda lol

--- carousel.js
+++ carousel.js
@@ -1,53 +1,29 @@
-// const slides = document.querySelectorAll('.slide');
-// let index = 0;
+// Path: carousel.js
+var slides = document.getElementsByClassName("slide");
+let index = 0;
 
-// function prevSlide(){
-//     slides[index].classList.remove('active');
-//     index--;
+function prevSlide() {
+    slides[index].classList.remove('active');
+    index--;
 
-//     if(index < 0)
-//         index = slides.length -1;
+    if (index < 0)
+        index = slides.length - 1;
 
-//     slides[index].classList.add('active');      
-// }
-
-// document.querySelector('.prev').addEventListener('click', e => {
-//     prevSlide();
-// });
-
-// function nextSlide(){
-//     slides[index].classList.remove('active');
-//     index++;
-
-//     if(index > slides.length -1)
-//         index = 0;
-
-//     slides[index].classList.add('active');      
-// }
-
-// document.querySelector('.next').addEventListener('click', e => {
-//     nextSlide();
-// });
-
-setTimeout(1000);
-slide();
-
-function slide(){
-    document.getElementById('slide')
-    setTimeout("slide1()", 3000);
+    slides[index].classList.add('active');
 }
 
-function slide1(){
-    document.getElementById('slide').style="background-image: url(assets/banners/spawn2.PNG);"
-    setTimeout("slide2()", 3000);
+function nextSlide() {
+    slides[index].classList.remove('active');
+    index++;
+
+    if (index > slides.length - 1)
+        index = 0;
+
+    slides[index].classList.add('active');
 }
 
-function slide2(){
-    document.getElementById('slide').style="background-image: url(assets/banners/spawn3.PNG);"
-    setTimeout("slide3()", 3000);
+function startCarousel() {
+    setInterval(nextSlide, 3000); // Change slide every 3 seconds (adjust as needed)
 }
 
-function slide3(){
-    document.getElementById('slide').style="background-image: url(assets/banners/spawn4.PNG);"
-    setTimeout("slide1()", 3000);
-}
+startCarousel(); // Start the automatic carousel movement
